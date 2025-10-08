d136c8361d828a476bfcd2e03ef7e05e1d92b583
aliiiu <ayokunumi99@gmail.com>
2025-10-08 22:52:17 +0100

    improve nav styles to use container

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,42 +1,76 @@
-import { Dialog, DialogBackdrop, DialogPanel, DialogTitle, TransitionChild } from "@headlessui/react"
-import Button from "./button"
-import { useState } from "react"
-import { LuX } from "react-icons/lu"
-import { href, Link } from "react-router"
-const links = [{
-  title: "Features",
-  href: "#",
-}, {
-  title: "Partners",
-  href: "#",
-}, {
-  title: "Contributions",
-  href: "#",
-}, {
-  title: "About Us",
-  href: "#", 
-}]
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+} from "@headlessui/react";
+import Button from "./button";
+import { useState } from "react";
+import { LuX } from "react-icons/lu";
+import { href, Link } from "react-router";
+const links = [
+  {
+    title: "Features",
+    href: "#",
+  },
+  {
+    title: "Partners",
+    href: "#",
+  },
+  {
+    title: "Contributions",
+    href: "#",
+  },
+  {
+    title: "About Us",
+    href: "#",
+  },
+];
 const Navigation = () => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <div className='hidden md:flex gap-[122px] py-6 px-25'>
-        <img src="/logo.png" alt="Logo" className='h-[58px] w-auto' />
-        <div className="flex gap-3.5 justify-center items-center">
-          <div className="flex gap-3.75 items-center justify-center">
-            {links.map(link => <Link to={link.href} key={link.title} className="text-[20px] leading-[120%] -tracking-[2%] min-w-[100px]">{link.title}</Link>)}
+      <div className="container flex items-center xl:gap-32 py-4 xl:py-6">
+        <img
+          src="/images/logo.png"
+          alt="Logo"
+          className="h-10 xl:h-[58px] w-auto"
+        />
+        <div className="hidden md:flex-1 md:flex gap-20 justify-between items-center">
+          <div className="flex flex-1 items-center justify-between">
+            {links.map((link) => (
+              <Link
+                to={link.href}
+                key={link.title}
+                className="text-[20px] leading-[120%] -tracking-[2%] min-w-[100px]"
+              >
+                {link.title}
+              </Link>
+            ))}
           </div>
           <div className="flex gap-3">
             <Button variant="outline">Sign In</Button>
             <Button>Get Started</Button>
           </div>
         </div>
-      </div>
-      <div className='md:hidden flex items-center justify-between p-4'>
-        <img src="/logo.png" alt="Logo" className='h-[40px] w-auto' />
-        <button onClick={() => setOpen(true)} className="text-gray-600 focus:outline-none">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+        <button
+          onClick={() => setOpen(true)}
+          className="md:hidden text-gray-600 focus:outline-none"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
           </svg>
         </button>
       </div>
@@ -69,11 +103,23 @@ const Navigation = () => {
                 <div className="relative flex h-full flex-col overflow-y-auto bg-white py-6 shadow-xl after:absolute after:inset-y-0 after:left-0 after:w-px after:bg-white/10">
                   <div className="px-4 sm:px-6">
                     <DialogTitle className="text-base font-semibold text-black">
-                      <img src="/logo.png" alt="Logo" className='h-[42px] w-auto' />
+                      <img
+                        src="/logo.png"
+                        alt="Logo"
+                        className="h-[42px] w-auto"
+                      />
                     </DialogTitle>
                   </div>
                   <div className="relative mt-6 flex-1 flex flex-col gap-4 px-4 sm:px-6">
-                    {links.map(link => <Link to={link.href} key={link.title} className="text-[20px] border-b border-b-neutral-300 px-4 py-2 leading-[120%] -tracking-[2%] min-w-[100px]">{link.title}</Link>)}
+                    {links.map((link) => (
+                      <Link
+                        to={link.href}
+                        key={link.title}
+                        className="text-[20px] border-b border-b-neutral-300 px-4 py-2 leading-[120%] -tracking-[2%] min-w-[100px]"
+                      >
+                        {link.title}
+                      </Link>
+                    ))}
                   </div>
                 </div>
               </DialogPanel>
@@ -82,8 +128,7 @@ const Navigation = () => {
         </div>
       </Dialog>
     </>
+  );
+};
 
-  )
-}
-
-export default Navigation
+export default Navigation;
