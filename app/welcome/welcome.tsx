e73df37574b9749c277f5ee17ac36063ec106f6e
aliiiu <ayokunumi99@gmail.com>
2025-10-09 11:31:38 +0100

    refactor(welcome): separate "The Solution" section for clarity

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -18,13 +18,6 @@ const scopes = [
     icon: PiUsers,
     color: "#CFAE37",
   },
-  {
-    title: "The Solution",
-    desc: "HealthScope bridges this gap. We partner with stakeholders in the health value chain to provide accessible health knowledge, making trusted health information easy to find, easy to understand, and easy to trust.",
-    icon: LuCircleCheckBig,
-    color: "#32A682",
-    bottom: ["Easy to Find", "Easy to Understand", "Easy to Trust"],
-  },
 ];
 export function Welcome() {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -33,47 +26,66 @@ export function Welcome() {
       <Navigation />
       <main className="flex flex-col flex-1">
         <Hero />
-        <div className="container flex flex-col gap-2 items-center md:py-12.5 py-4">
-          <h2 className="font-semibold text-2xl md:text-[40px] leading-[120%] -tracking-[4%]">
+        <div className="">
+          <h2 className="font-semibold text-center text-2xl md:text-[40px]">
             Why HealthScope Matters
           </h2>
-          <div className="flex flex-col">
-            <div className="md:grid flex flex-col md:grid-cols-2 gap-9">
-              {scopes.map((scope, index) => (
+          <div className="">
+            <div className="container py-4 md:py-12 grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-9">
+              {scopes.map((scope) => (
                 <div
-                  className={`md:py-10 p-2 md:px-7.5 w-full ${
-                    index === 2 ? "col-span-2" : "border bg-white "
-                  } rounded-md border-[#D5D5D5] gap-3.75`}
+                  className={`md:py-10 p-2 md:px-7.5 w-full rounded-md border border-[#D5D5D5] gap-3.75`}
                   key={scope.title}
                 >
                   <div className="flex gap-1.25 items-center">
                     <div
-                      className="size-12 rounded-sm flex items-center justify-center"
+                      className="size-10 md:size-12.5 rounded-sm flex items-center justify-center"
                       style={{ backgroundColor: scope.color + "10" }}
                     >
-                      <scope.icon color={scope.color} size={24} />
+                      <scope.icon
+                        color={scope.color}
+                        className="text-2xl md:text-4xl"
+                      />
                     </div>
-                    <h3 className="font-semibold text-lg">{scope.title}</h3>
+                    <h3 className="font-semibold text-lg md:text-[28px]">
+                      {scope.title}
+                    </h3>
                   </div>
-                  <p className="mt-2 text-base text-[#797979] leading-[160%] -tracking-[2%]">
+                  <p className="mt-2 text-base md:text-2xl text-gray-500">
                     {scope.desc}
                   </p>
-
-                  <div className="flex flex-wrap gap-2 md:gap-10 mt-2 md:mt-6">
-                    {scope.bottom?.map((item, index) => (
-                      <div className="gap-1 flex bg-white rounded-full px-3.75 py-2 border border-[#D5D5D5] items-center">
-                        <LuCircleCheckBig
-                          color="#32A682"
-                          size={isMobile ? 16 : 21}
-                        />
-                        <span className="text-sm md:text-xl leading-[120%] tracking-[-2%]">
+                </div>
+              ))}
+            </div>
+            <div className="bg-gray-50 py-4 md:py-12">
+              <div className="container">
+                <div className="flex gap-1.25 items-center">
+                  <div className="size-10 md:size-12.5 rounded-sm flex items-center justify-center bg-green-50">
+                    <LuCircleCheckBig className="text-green-500 text-2xl mdtext-4xl" />
+                  </div>
+                  <h3 className="font-semibold  text-lg md:text-[28px]">
+                    The Solution
+                  </h3>
+                </div>
+                <p className="mt-2 text-base md:text-2xl text-gray-500 max-w-[1111px]">
+                  HealthScope bridges this gap. We partner with stakeholders in
+                  the health value chain to provide accessible health knowledge,
+                  making trusted health information easy to find, easy to
+                  understand, and easy to trust.
+                </p>
+                <div className="flex flex-wrap gap-2 md:gap-10 mt-2 md:mt-6">
+                  {["Easy to Find", "Easy to Understand", "Easy to Trust"].map(
+                    (item) => (
+                      <div className="gap-3 flex bg-white rounded-full px-3.75 py-2 border border-gray-100 items-center">
+                        <LuCircleCheckBig className="text-green-500 text-sm md:text-2xl" />
+                        <span className="text-sm md:text-xl font-medium">
                           {item}
                         </span>
                       </div>
-                    ))}
-                  </div>
+                    )
+                  )}
                 </div>
-              ))}
+              </div>
             </div>
           </div>
         </div>
