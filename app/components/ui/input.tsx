c50b2601caeac83dd5ff4e1d7855b1f8ae49acfd
aliiiu <ayokunumi99@gmail.com>
2025-10-11 13:01:23 +0100

    style(input): update input background and text color for consistency

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "h-12 w-full min-w-0 rounded-lg border border-transparent bg-[#F3F3F5] px-4 py-3 text-base text-[#333333] placeholder:text-[#949494] transition focus-visible:bg-white focus-visible:ring-2 focus-visible:ring-[#4C7DEA]/30 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-60",
+        "h-12 w-full min-w-0 rounded-lg border border-transparent bg-input px-4 py-3 text-base text-gray-900 placeholder:text-[#949494] transition focus-visible:bg-white focus-visible:ring-2 focus-visible:ring-[#4C7DEA]/30 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-60",
         "file:inline-flex file:h-9 file:rounded-md file:border-0 file:bg-transparent file:px-3 file:text-sm file:font-medium",
         "selection:bg-primary selection:text-primary-foreground",
         className
@@ -18,4 +18,4 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   );
 }
 
-export { Input }
+export { Input };
