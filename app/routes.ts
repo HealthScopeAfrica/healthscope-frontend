48d46c207a71aea2bd334dfb0aab7af5967341cd
aliiiu <ayokunumi99@gmail.com>
2025-10-11 09:49:26 +0100

    refactor(routes): update dashboard route paths to use nested directory

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
 	index("routes/home.tsx"),
 	route("partner/login", "partner/login/partner.tsx"),
-	route("dashboard", "routes/dashboard.tsx"),
-	route("dashboard/profile", "routes/profile.tsx"),
+	route("dashboard", "routes/(dashboard)/dashboard.tsx"),
+	route("dashboard/profile", "routes/(dashboard)/profile.tsx"),
 ] satisfies RouteConfig;
